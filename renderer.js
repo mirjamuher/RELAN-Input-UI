cefc35a1f2810a9928d9d0a5a63a0c90ae809542
Mirjam Uher <mirjam.uher@digio.com.au>
2022-09-29 17:07:18 +1000

    start implementing jQuery to make the form responsive to  user input

--- renderer.js
+++ renderer.js
@@ -4,3 +4,32 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+$(document).ready(function(){
+    // jQuery methods go here
+    //$("#option").prop('disabled', true);
+
+    // 1. depending on which Option is selected, certain parameters are disabled
+    $('#option').on('change', function() {
+        if ($('#option').val() === "0") {
+           alert("CONNECTIONS") 
+        } 
+
+        else if ($('#option').val() === "1") {
+            alert("IMPLICATIONS") 
+        } 
+
+        else if ($('#option').val() === "2") {
+            alert("CONFIRMATION") 
+            // level of simulation, inclusion criterion, graph theoretical analysis
+            $('#simParameter').prop('disabled', true);
+            $('#inclusionCriteria').prop('disabled', true);
+            $('#graphAnalysis').prop('disabled', true);
+            // todo: have text under field update to say "disabled for option CONFIRMATION" or something
+        } 
+
+        else if ($('#option').val() === "3") {
+            alert("SIMULATION") 
+        } 
+    })
+});
