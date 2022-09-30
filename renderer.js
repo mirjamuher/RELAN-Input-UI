9e5cb1aee3f749e162c39a86dbe8a749db8949be
Mirjam Uher <mirjam.uher@digio.com.au>
2022-10-01 09:52:17 +1000

    switch from jQuery to JS

--- renderer.js
+++ renderer.js
@@ -5,66 +5,66 @@
 // selectively enable features needed in the rendering
 // process.
 
-$(document).ready(function(){
-    // jQuery methods go here
-    //$("#option").prop('disabled', true);
-
+document.addEventListener('DOMContentLoaded', (event) => {
     // Whenever a different option is selected, certain parameters are hidden
-    $('#option').on('change', function() {
-        // have everything reappear upon change of option
-        $('div:hidden').each(function() {
-            $(this).show();
+    const option = document.getElementById('option');
+    option.addEventListener('change', function handleOptionSelection(event) {
+        // have everything reappear 
+        document.querySelectorAll('.hidden').forEach(el => {
+            el.classList.remove('hidden');
         })
-        
+
         // hide unnecessary parameters from user
         // CONNECTIONS
-        if ($('#option').val() === "0") {
-           $('#simParameterDiv').hide();
-           $('#extractParamDiv').hide();
-           $('#extractionModelDiv').hide();
-           $('#inclusionCriteriaDiv').hide();
-           $('#yCutDiv').hide();
-           $('#xCutDiv').hide();
-           $('#graphAnalysisDiv').hide();
-           $('#truthFuncDiv').hide();
-           $('#hypoFuncDiv').hide();
-           $('#extraFuncDiv').hide();
-        } 
+        if (event.target.value === '0') {
+            console.log(event)
+            document.getElementById('simParameterDiv').classList.add('hidden');
+            document.getElementById('extractParamDiv').classList.add('hidden');
+            document.getElementById('extractionModelDiv').classList.add('hidden');
+            document.getElementById('inclusionCriteriaDiv').classList.add('hidden');
+            document.getElementById('yCutDiv').classList.add('hidden');
+            document.getElementById('xCutDiv').classList.add('hidden');
+            document.getElementById('graphAnalysisDiv').classList.add('hidden');
+            document.getElementById('truthFuncDiv').classList.add('hidden');
+            document.getElementById('hypoFuncDiv').classList.add('hidden');
+            document.getElementById('extraFuncDiv').classList.add('hidden');
+        }
 
-        // IMPLICATIONS
-        else if ($('#option').val() === "1") {
-            $('#simParameterDiv').hide();
-            $('#extractParamDiv').hide();
-            $('#extractionModelDiv').hide();
-            $('#inclusionCriteriaDiv').hide();
-            $('#yCutDiv').hide();
-            $('#xCutDiv').hide();
-            $('#truthFuncDiv').hide();
-            $('#hypoFuncDiv').hide();
-            $('#extraFuncDiv').hide();
-        } 
+        // IMPLICATION
+        else if (event.target.value === '1') {
+            console.log(event)
+            document.getElementById('simParameterDiv').classList.add('hidden');
+            document.getElementById('extractParamDiv').classList.add('hidden');
+            document.getElementById('extractionModelDiv').classList.add('hidden');
+            document.getElementById('inclusionCriteriaDiv').classList.add('hidden');
+            document.getElementById('yCutDiv').classList.add('hidden');
+            document.getElementById('xCutDiv').classList.add('hidden');
+            document.getElementById('truthFuncDiv').classList.add('hidden');
+            document.getElementById('hypoFuncDiv').classList.add('hidden');
+            document.getElementById('extraFuncDiv').classList.add('hidden');
+        }
 
         // CONFIRMATION
-        else if ($('#option').val() === "2") {
-            // level of simulation, inclusion criterion, graph theoretical analysis
-            $('#simParameterDiv').hide();
-            $('#inclusionCriteriaDiv').hide();
-            $('#yCutDiv').hide();
-            $('#xCutDiv').hide();
-            $('#graphAnalysisDiv').hide();
+        else if (event.target.value === '2') {
+            console.log(event)
+            document.getElementById('simParameterDiv').classList.add('hidden');
+            document.getElementById('inclusionCriteriaDiv').classList.add('hidden');
+            document.getElementById('yCutDiv').classList.add('hidden');
+            document.getElementById('xCutDiv').classList.add('hidden');
+            document.getElementById('graphAnalysisDiv').classList.add('hidden');
         }
 
         // SIMULATION
-        else if ($('#option').val() === "3") {
-            alert("SIMULATION") 
-            $('#dataInputDiv').hide();
-            $('#dataFormatDiv').hide();
-            $('#dichtomisationDiv').hide();
-            $('#vectorOfDichtomisationDiv').hide();
-            $('#inclusionCriteriaDiv').hide();
-            $('#yCutDiv').hide();
-            $('#xCutDiv').hide();
-            $('#graphAnalysisDiv').hide();
-        } 
+        else if (event.target.value === '3') {
+            console.log(event)
+            document.getElementById('dataInputDiv').classList.add('hidden');
+            document.getElementById('dataFormatDiv').classList.add('hidden');
+            document.getElementById('dichtomisationDiv').classList.add('hidden');
+            document.getElementById('vectorOfDichtomisationDiv').classList.add('hidden');
+            document.getElementById('inclusionCriteriaDiv').classList.add('hidden');
+            document.getElementById('yCutDiv').classList.add('hidden');
+            document.getElementById('xCutDiv').classList.add('hidden');
+            document.getElementById('graphAnalysisDiv').classList.add('hidden');
+        }
     })
 });
