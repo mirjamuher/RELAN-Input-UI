12c44636ac040087937d9c4483d6f7553df80576
Mirjam Uher <mirjam.uher@digio.com.au>
2022-12-08 09:00:43 +1100

    change truth function logic and default value to be option dependant

--- logic/handle_option_selection.js
+++ logic/handle_option_selection.js
@@ -19,26 +19,24 @@ function handleOptionSelection(optionValue) {
         document.getElementById('simParameterDiv').classList.add('hidden');
         document.getElementById('extractParamDiv').classList.add('hidden');
         document.getElementById('extractionModelDiv').classList.add('hidden');
-        document.getElementById('truthFuncDiv').classList.add('hidden');
         document.getElementById('hypoFuncDiv').classList.add('hidden');
         document.getElementById('extraFuncDiv').classList.add('hidden');
 
         // Connection-specific selection logic
-        let inclusionCriteria = document.getElementById('inclusionCriteria');
-        inclusionCriteria.value = 1;
+        document.getElementById('inclusionCriteria').value = 1;
     }
     // IMPLICATION
     else if (optionValue === '1') {
         document.getElementById('simParameterDiv').classList.add('hidden');
         document.getElementById('extractParamDiv').classList.add('hidden');
         document.getElementById('extractionModelDiv').classList.add('hidden');
-        document.getElementById('truthFuncDiv').classList.add('hidden');
+        //document.getElementById('truthFuncDiv').classList.add('hidden');
         document.getElementById('hypoFuncDiv').classList.add('hidden');
         document.getElementById('extraFuncDiv').classList.add('hidden');
 
         // Implication-specific selection logic
-        let inclusionCriteria = document.getElementById('inclusionCriteria');
-        inclusionCriteria.value = 0;
+        document.getElementById('inclusionCriteria').value = 0;
+        document.getElementById('truthFunc').value = 0;
     }
     // CONFIRMATION
     else if (optionValue === '2') {
@@ -57,7 +55,12 @@ function handleOptionSelection(optionValue) {
         document.getElementById('yCutDiv').classList.add('hidden');
         document.getElementById('xCutDiv').classList.add('hidden');
         document.getElementById('graphAnalysisDiv').classList.add('hidden');
-    } else {
+
+        // simulation-specific selection logic
+        document.getElementById('truthFunc').value = 0;
+    } 
+    
+    else {
         console.error(`Error: invalid option value selected: ${optionValue}`);
         alert("Unknown error occurred. Please restart program. Contact Dr. Maderthaner if problem persists");
     }
